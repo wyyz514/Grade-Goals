eb3ae2ab488c434377d581f27caedd2d67ab8716
WoleObayomi <oluwole.obayomi@mail.mcgill.ca>
2015-08-03 02:22:08 -0400

    Reloading caused the app to crash sometimes since the message wasn't actually passed. Added a promise to fix that

--- background.js
+++ background.js
@@ -7,19 +7,25 @@ chrome.tabs.onUpdated.addListener(function(tabId,changeInfo,tab){
 chrome.runtime.onMessage.addListener(function(msg,sender,sendResp){
   if(msg.message == "LOAD")
   {
-    var resp = "";
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET",chrome.extension.getURL("gg.html"),true);
-    xhr.onreadystatechange = function(){
-      if(xhr.readyState == 4)
-      {
-        resp = xhr.responseText;
-      }
-    };
-    xhr.send();
-    chrome.tabs.query({active:true,currentWindow:true},function(tabs){
-      chrome.tabs.sendMessage(tabs[0].id,{message:resp},function(response){
+    var promise = new Promise(function(resolve,reject){
+      var resp = "";
+      var xhr = new XMLHttpRequest();
+      xhr.open("GET",chrome.extension.getURL("gg.html"),true); 
+      xhr.onreadystatechange = function(){
+        if(xhr.readyState == 4)
+        {
+          resp = xhr.responseText;
+          resolve(resp);
+        }
+      };
+      xhr.send();
+      return promise;
+    })
+    .then(function(resp){
+      chrome.tabs.query({active:true,currentWindow:true},function(tabs){
+        chrome.tabs.sendMessage(tabs[0].id,{message:resp},function(response){
       
+        });
       });
     });
   } 
